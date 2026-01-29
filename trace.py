71f18fbad777671225dcd4bf5a22c5957eb91083
Yuriy <ko10ok@users.noreply.github.com>
2026-01-30 00:37:03 +0300

    Update trace.py

--- trace.py
+++ trace.py
@@ -1,20 +1,17 @@
 import sys
-from time import time
-
 from dataclasses import dataclass
 
 
-@dataclass
-class Record:
-    start_s: float
-    func_name: str
-    filename: str
-    line_number: int
-
-
 class FunctionInterceptor:
+    @dataclass
+    class Record:
+        start_s: float
+        func_name: str
+        filename: str
+        line_number: int
+
     def __init__(self, include_file_fn=None, min_time_ms=10):
-        self.call_stack: list[Record] = []
+        self.call_stack: list[FunctionInterceptor.Record] = []
 
         self.include_file_fn = include_file_fn
         if self.include_file_fn is None:
@@ -27,6 +24,9 @@ class FunctionInterceptor:
 
         self.min_time_ms = min_time_ms
 
+        from time import time
+        self.system_time = time
+
     def trace_calls(self, frame, event, arg):
         filename = frame.f_code.co_filename
         should_log = self.include_file_fn(filename)
@@ -38,18 +38,18 @@ class FunctionInterceptor:
             line_number = frame.f_lineno
 
             # print(f" > CALL: {func_name} at {filename}:{line_number}")
-            self.call_stack.append(Record(time(), func_name, filename, line_number))
+            self.call_stack.append(FunctionInterceptor.Record(self.system_time(), func_name, filename, line_number))
 
         elif event == 'return' and should_log:
 
             func_name = frame.f_code.co_name
             filename = frame.f_code.co_filename
             if self.call_stack and self.call_stack[-1].func_name == func_name:
-                if int(time() * 1000 - self.call_stack[-1].start_s * 1000) >= min_time_ms:
+                if int(self.system_time() * 1000 - self.call_stack[-1].start_s * 1000) >= min_time_ms:
                     verbose = f'-> {arg}' if False else ''
                     a = "| "
                     print(
-                        f" > {int(time() * 1000 - self.call_stack[-1].start_s * 1000):>10} ms :: "
+                        f" > {int(self.system_time() * 1000 - self.call_stack[-1].start_s * 1000):>10} ms :: "
                         f"{a * len(self.call_stack)}{func_name} {verbose} ::: {filename}"
                     )
                 self.call_stack.pop()
